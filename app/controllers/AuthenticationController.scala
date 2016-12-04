95476bab5254ecbd861f31628ad7ebffcf5c091e
Sadat Anwar <s.anwar@rebuy.com>
2016-12-05 00:39:43 +0100

    clean up

--- app/controllers/AuthenticationController.scala
+++ app/controllers/AuthenticationController.scala
@@ -3,23 +3,14 @@ package controllers
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 import com.google.inject.Inject
 import models.Formats._
-import models.{NewUserDto, UserIdentity}
+import models.UserIdentity
 import services.{AuthenticationService, UserIdentityService}
 
 class AuthenticationController @Inject()(authenticationService: AuthenticationService) extends Controller {
-
-  def signUp() = Action.async(parse.json) { request =>
-    authenticationService.signUp(request.body.as[NewUserDto]).map {
-      userIdentityStore =>
-        val user: UserIdentity = UserIdentityService.map(userIdentityStore)
-        Created(Json.toJson(user)).withSession("email" -> user.email)
-    }
-  }
 
   def authenticate() = Action.async(parse.json) { request =>
     val identity: UserIdentity = request.body.as[UserIdentity]
