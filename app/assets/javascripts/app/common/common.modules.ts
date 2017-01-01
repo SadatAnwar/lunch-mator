853a65895de45c73afcb2a85a30d006225323712
Sadat Anwar <s.anwar@rebuy.com>
2017-01-01 01:13:27 +0100

    added about page

--- app/assets/javascripts/app/common/common.modules.ts
+++ app/assets/javascripts/app/common/common.modules.ts
@@ -5,11 +5,12 @@ import {routing} from 'app/authentication/authentication.routing';
 import {ErrorDetailComponent} from 'app/common/components/error/error-detail.component';
 import {AlertComponent} from './components/alert/alert.component';
 import {WelcomeComponent} from './components/welcome/welcome.component';
+import {AboutComponent} from './components/about/about.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing],
-  declarations: [ErrorDetailComponent, AlertComponent, WelcomeComponent],
-  exports: [ErrorDetailComponent, AlertComponent, WelcomeComponent]
+  declarations: [ErrorDetailComponent, AlertComponent, AboutComponent, WelcomeComponent],
+  exports: [ErrorDetailComponent, AlertComponent, AboutComponent, WelcomeComponent]
 })
 export class CommonModules {
 }
