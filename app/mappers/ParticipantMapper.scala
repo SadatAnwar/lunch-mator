86c95a3bdb5463a88ac4eedfed4f5dc5ad36876a
Sadat Anwar <s.anwar@rebuy.com>
2017-02-04 22:09:23 +0100

    Anonymous mode working

--- app/mappers/ParticipantMapper.scala
+++ app/mappers/ParticipantMapper.scala
@@ -4,7 +4,13 @@ import models._
 
 object ParticipantMapper {
 
-  def map(participantRow: ParticipantRow, userRow: UserRow): ParticipantDto = {
-    ParticipantDto(userRow.firstName, userRow.lastName, participantRow.joined)
+  def map(participantRow: ParticipantRow, userRow: UserRow, lunchRow: LunchRow): ParticipantDto = {
+    var firstName = userRow.firstName
+    var lastName = userRow.lastName
+    if (lunchRow.anonymous) {
+      firstName = "Anonymous"
+      lastName = "User"
+    }
+    ParticipantDto(firstName, lastName, participantRow.joined)
   }
 }
